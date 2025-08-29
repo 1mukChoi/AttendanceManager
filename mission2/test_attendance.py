2b53106f26b219f9fd676a9c15c39947bbb61c1e
1mukChoi <reason108@naver.com>
2025-08-29 14:17:55 +0900

    [Unittest] add_player

---
+++ mission2/test_attendance.py
@@ -0,0 +1,17 @@
+import pytest
+from pytest_mock import MockerFixture
+
+from attendance import Attendance
+
+
+def test_add_member(mocker: MockerFixture):
+    name_list = ["Kevin", "Sunny", "James", "Steven"]
+    mocker.patch('player.Player', return_value=name_list)
+    attendance = Attendance()
+    for index, name in enumerate(name_list):
+        attendance.add_player(name)
+
+        assert attendance.player_index_dict[name] == index
+        assert len(attendance.player_list) == index + 1
+
+    assert attendance.player_index_dict == {"Kevin": 0, "Sunny": 1, "James": 2, "Steven": 3}
