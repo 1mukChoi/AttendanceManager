21adcb890274a5e4968f62785cea64277c3dfc71
1mukChoi <reason108@naver.com>
2025-08-29 13:01:20 +0900

    [Refactoring] use pytest.fixture

--- mission2/test_member.py
+++ mission2/test_member.py
@@ -3,9 +3,12 @@ from pytest_mock import MockerFixture
 
 from mission2.member import Member
 
+@pytest.fixture
+def make_member():
+    return Member("Kevin")
 
-def test_member_init():
-    member = Member("Kevin")
+def test_member_init(make_member):
+    member = make_member
     assert member.name == "Kevin"
     assert member.id == Member.LAST_ID
     assert member.points == 0
@@ -14,8 +17,8 @@ def test_member_init():
     assert member.attend_num_weekend == 0
 
 @pytest.mark.parametrize("attendance_day, points, attend_num_wednesday, attend_num_weekend", [("monday", 1, 0, 0), ("tuesday", 1, 0, 0), ("wednesday", 3, 1, 0), ("thursday", 1, 0, 0), ("friday", 1, 0, 0), ("saturday", 2, 0, 1), ("sunday", 2, 0, 1)])
-def test_attend(attendance_day, points, attend_num_wednesday, attend_num_weekend):
-    member = Member("Kevin")
+def test_attend(make_member, attendance_day, points, attend_num_wednesday, attend_num_weekend):
+    member = make_member
     member.attend(attendance_day)
     assert member.points == points
     assert member.attend_num_wednesday == attend_num_wednesday
@@ -23,8 +26,8 @@ def test_attend(attendance_day, points, attend_num_wednesday, attend_num_weekend
 
 
 @pytest.mark.parametrize("points, grade", [(0, "NORMAL"), (10, "NORMAL"), (20, "NORMAL"), (30, "SILVER"), (40, "SILVER"), (50, "GOLD"), (100, "GOLD")])
-def test_update_grade(points, grade, mocker: MockerFixture):
-    member = Member("Kevin")
+def test_update_grade(make_member, points, grade, mocker: MockerFixture):
+    member = make_member
     member.points = points
     mocked_print = mocker.patch("builtins.print")
 
